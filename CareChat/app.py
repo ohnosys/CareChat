57ea361653d16746d6b85feac49bdf759d298619
ohno <36059171+ohnosys@users.noreply.github.com>
2018-02-08 20:25:31 +0900

    Update app.py

--- CareChat/app.py
+++ CareChat/app.py
@@ -3,7 +3,7 @@
 
 from __future__ import print_function # Python2系でも動くように
 import os # パスを扱うため
-import json 
+import json
 import random
 import tornado.httpserver
 import tornado.ioloop
@@ -11,15 +11,12 @@ import tornado.web
 import tornado.websocket
 from tornado.web import url
 
-# Tornadoサーバー / 全体の流れ
-    # Handlerをクラスとして作成（get post ...）
-    # AplicationクラスにルーディングとHandlerクラスを紐づけしつつ設定を書き込む
-    # サーバー用のオプションをパースしてPortとか設定してサーバースタート！
-
 
 class IndexHandler(tornado.web.RequestHandler):
 
     def get(self, *args, **kwargs): # *はtuple、**はdictionaryとして変数管理
+        # GETリクエストの際に呼ばれるメソッド
+
         face_pics = ['cat1.gif', 'cat2.gif', 'cat3.gif'] # ユーザー管理画面で固有アイコン管理ができるようにしたい
         img_name = random.choice(face_pics) # face_picsからいずれか取り出す
         self.render('index.html', img_path=self.static_url('images/' + img_name)) # 相対パスをURLに変換 /static/images/img_name
@@ -28,24 +25,27 @@ class IndexHandler(tornado.web.RequestHandler):
 class ChatHandler(tornado.websocket.WebSocketHandler):
 
     # waitersとmessagesに接続している人と送られてきたメッセージを記録
-    waiters = set()
-    messages = []
+    waiters = set() # 集合を表すデータ型で複数要素持てる / 順不同、重複は除外、dic型はkeyを保持
+    messages = []  # リスト
 
     def open(self, *args, **kwargs):
-        # 接続してきた人の登録と、その人に対して今までのログを送信
+        # base.html でコネクションが確保されると呼び出される
+        # 接続してきた人の登録と、その人に対して今までのmessageログを送信
         self.waiters.add(self)
         self.write_message({'messages': self.messages})
 
     def on_message(self, message):
+        # クライアントからメッセージが送られてくると呼び出される
         # メッセージが送られてきたときに送られてきたメッセージを自分以外の参加者にブロードキャスト / 送られてきたメッセージはログに追加
         message = json.loads(message)
-        self.messages.append(message) # messagesにdic型のmessageを追加
+        self.messages.append(message) # messagesにdic型のmessageを末尾追加
         for waiter in self.waiters:
             if waiter == self:
                 continue
             waiter.write_message({'img_path': message['img_path'], 'message': message['message']})
-
+　
     def on_close(self):
+        # ページが閉じてコネクションが切れることで呼び出される / ループを終了
         # 接続が切断されたときにwaitersから接続者を削除 / 切断された人にはメッセージをブロードキャストしないようにする
         self.waiters.remove(self)
 
@@ -60,13 +60,13 @@ class Application(tornado.web.Application):
                                          url(r'/', IndexHandler, name='index'), # URLの逆引きができるように chat → /chat
                                          url(r'/chat', ChatHandler, name='chat'),
                                          ],
-                                         template_path=os.path.join(BASE_DIR, 'templates'), # htmlテンプレのフォルダの設定 
+                                         template_path=os.path.join(BASE_DIR, 'templates'), # htmlテンプレのフォルダの設定
                                          static_path=os.path.join(BASE_DIR, 'static'), # jsやcssなどを入れておくフォルダの設定
                                          )
 
 if __name__ == '__main__':
     # サーバーを立ち上げる処理
-    app = Application() 
+    app = Application()
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
