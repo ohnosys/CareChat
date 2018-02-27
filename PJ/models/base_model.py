71e3bc0197dd31c3d47db1654354edc49d056fcb
ohno <36059171+ohnosys@users.noreply.github.com>
2018-02-27 19:08:58 +0900

    Add files via upload

--- PJ/models/base_model.py
+++ PJ/models/base_model.py
@@ -5,15 +5,29 @@ from backend import Backend
 
 Base = declarative_base()
 
+# models.py は モデルをORMで書くところ
+# モデルの設定ファイル↓
+# Django は models.py
+# Trnado は modelsディレクトリ内の各ファイル
+# view.py は 長くなりすぎるので分割したりロジックだけ別コードに書き出したりしている
 
 class DjangoLikeModelMixin(object):
+    # Base = declarative_base を拡張したい
+    # djangolikeなmodel定義にするmixin    
+
+    # dclarative_base()で生成したクラスオブジェクトはモデルを書くときにテーブル名と主キーを書かないとAlchemyさんに怒られる
+
+    # id は全てのクラスで使うので
     id = Column(Integer, primary_key=True)
 
-    @declared_attr
+    # 例えば __tablename__ = "user" と同じ（定義するのが面倒くさいから）
+    # __tablename__ == user.py の class User を入れて小文字にしてる
+    
+    @declared_attri # Foreign KeyなどをMixinで定義するときに付ける必要があるデコレータ
     def __tablename__(cls):
         return cls.__name__.lower()
 
-    @classmethod
+    @classmethod # クラス変数にアクセス / 動作が変わるべきとき
     def get(cls, id):
         return cls.session().query(cls).get(id)
 
@@ -29,7 +43,7 @@ class DjangoLikeModelMixin(object):
         self.session().delete(self)
         self.session().commit()
 
-    @staticmethod
+    @staticmethod # 継承クラスでも動作変わらない
     def session():
         return Backend.instance().get_session()
 
